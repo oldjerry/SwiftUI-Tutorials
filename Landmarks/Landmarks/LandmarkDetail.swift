840b7b056f3b2e6ade9ba54e6ea66c39b860b2e7
Jerry Sun <oldjerry.m@gmail.com>
2020-02-10 15:09:30 +0800

    Finished Animating Views and Transitions

--- Landmarks/Landmarks/LandmarkDetail.swift
+++ Landmarks/Landmarks/LandmarkDetail.swift
@@ -9,7 +9,15 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    
+    @EnvironmentObject var userData: UserData
     var landmark: Landmark
+    
+    //
+    var landmarkIndex: Int {
+        userData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    //
     
     var body: some View {
         VStack {
@@ -22,8 +30,18 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    Button(action: {self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()}) {
+                        self.userData.landmarks[self.landmarkIndex].isFavorite ?
+                                Image(systemName: "star.fill")
+                            .foregroundColor(Color.yellow) :
+                                Image(systemName: "star")
+                                .foregroundColor(Color.gray)
+                    }
+                    
+                }
                 
                 HStack {
                     Text(landmark.park)
@@ -46,5 +64,6 @@ struct LandmarkDetail: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkDetail(landmark: landmarkData[0])
+        .environmentObject(UserData())
     }
 }
---
+++ Landmarks/Landmarks/Models/UserData.swift
@@ -0,0 +1,19 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Jerry Sun on 2020/2/10.
+//  Copyright © 2020 Jerry Sun. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+//
+import Combine
+
+final class UserData: ObservableObject  {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+
+}
+//
