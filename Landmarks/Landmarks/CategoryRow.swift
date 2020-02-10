b8b7ea544850d1adcd868947e0c7aa8723671f02
Jerry Sun <oldjerry.m@gmail.com>
2020-02-10 17:18:17 +0800

    Finish Composing Complex Interfaces

---
+++ Landmarks/Landmarks/CategoryRow.swift
@@ -0,0 +1,69 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Jerry Sun on 2020/2/10.
+//  Copyright © 2020 Jerry Sun. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+                Text(self.categoryName)
+                    .font(.headline)
+                    .padding(.leading, 15)
+                    .padding(.top, 5)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 0) {
+                        ForEach(self.items) { landmark in
+                            NavigationLink(
+                                destination: LandmarkDetail(
+                                    landmark: landmark
+                                )
+                            ) {
+                                CategoryItem(landmark: landmark)
+                            }
+                        }
+                    }
+                }
+                .frame(height: 185)
+            }
+        }
+}
+
+//
+struct CategoryItem: View {
+    var landmark: Landmark
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .renderingMode(.original) // for show item image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .foregroundColor(.primary) // for show item text
+                .font(.caption)
+            
+//            Change the navigation appearance of the category items by applying the renderingMode(_:) and foregroundColor(_:) modifiers.
+        }
+        .padding(.leading, 15)
+    }
+}
+//
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarkData[0].category.rawValue,
+            items: Array(landmarkData.prefix(4))
+        )
+    }
+}
--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -13,7 +13,7 @@ struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
     
     var body: some View {
-        NavigationView {
+     //   NavigationView {
             List {
                 Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites only")
@@ -29,16 +29,18 @@ struct LandmarkList: View {
             }
             .navigationBarTitle(Text("Landmarks"))
         }
-    }
+  //  }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone SE", "iPhone 11 Pro"], id: \.self) { deviceName in
-            LandmarkList()
-                .environmentObject(UserData())
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
+            NavigationView {
+                LandmarkList()
+                    .environmentObject(UserData())
+                    .previewDevice(PreviewDevice(rawValue: deviceName))
+                    .previewDisplayName(deviceName)
+            }
         }
     }
 }
